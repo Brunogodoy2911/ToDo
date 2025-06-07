5cf71cb5c96f98626954e839361348f8511bbb0d
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-06-07 14:51:09 -0300

    :sparkles: Criando componente ButtonIcon

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import XIcon from "./assets/icons/X-Regular.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 export default function App() {
   return (
@@ -28,6 +29,12 @@ export default function App() {
 
       <div className="flex gap-2">
         <Button icon={PlusIcon}>Nova Tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
       </div>
     </div>
   );

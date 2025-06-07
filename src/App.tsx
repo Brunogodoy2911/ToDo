478682946975c43ca5b1a4cc7ae93ba31411794c
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-06-06 23:01:12 -0300

    :sparkles: Criação do componente Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import "./App.css";
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "./assets/icons/Check-Regular.svg?react";
+import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
+import SpinnerIcon from "./assets/icons/Spinner.svg?react";
+import XIcon from "./assets/icons/X-Regular.svg?react";
+import Icon from "./components/Icon";
 
 export default function App() {
-  return <div className="text-pink-dark">Olá mundo</div>;
+  return (
+    <div className="grid gap-3">
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={XIcon} />
+      </div>
+    </div>
+  );
 }
